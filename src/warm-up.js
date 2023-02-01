062a28575fa4eaacdbfcf5659a160ff05185d9df
“Sarah <sarahcatherine311@gmail.com>
2023-02-01 16:09:32 -0700

    Finish warm  up

--- src/warm-up.js
+++ src/warm-up.js
@@ -57,6 +57,14 @@ console.log(shouldOrderMore);
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
 // the statement 'I'm going to eat some [food]!', otherwise, log the statement 
 // 'We need to get some more [food].'
+var stockCount = 3
+
+
+if (stockCount >= 1) {
+    console.log(`I'm going to eat some ${food}!`);
+} else {
+    console.log(`We need to get some more ${food}.`);
+}
 
 
 
@@ -65,6 +73,11 @@ console.log(shouldOrderMore);
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
+if(isFavorite === true) {
+    console.log(`My favorite food is ${food}!`);
+} else {
+    console.log(`I recently ate ${food}`);
+}
 
 
 //------------------------- Arrays -------------------------//
@@ -73,17 +86,21 @@ console.log(shouldOrderMore);
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
 
+var vegetables = ["onions", "broccoli", "asparagus", "brussel sprouts", "carrots"];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
 
+console.log(vegetables[2]);
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
 
+var fruits = ["apples", "oranges", "strawberries", "blueberries", "bananas", "grapes", "raspberries"];
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
+console.log(fruits[4]);
 
 //------------------------- Objects -------------------------//
 
@@ -91,7 +108,16 @@ console.log(shouldOrderMore);
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
 
+var groceryStore = {
+    name: "King Soopers",
+    isOpen: true,
+}
+// Other key value pairs that would make sense would be address: "address", phone number: "number", etc.
 
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+groceryStore.location = "Denver, Colorado"
+
+console.log(groceryStore);

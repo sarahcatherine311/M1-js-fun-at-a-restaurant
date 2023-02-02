ee024006569969b009eb2461e7d4afdcc7d9e7d9
“Sarah <sarahcatherine311@gmail.com>
2023-02-01 18:08:27 -0700

    Complete first 3 functions in meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,28 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem() {
+  menuItem = {
+    name: "Delicious French Toast",
+    price: 10.99,
+    type: "breakfast",
+  }
+  return menuItem
+}
+
+function addIngredients(name, ingredients) {
+  // for (var i = 0; i < ingredients.length; i++) {
+    // if (`{name} !== ingredients[i]`) {
+      return ingredients.push(name);
+    // }
+  //  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe

d9b9e3c0805eea2de8da233ac1eedbc77c26c3b9
“Sarah <sarahcatherine311@gmail.com>
2023-02-03 09:10:57 -0700

    Show work completed on order-test.js

--- src/order.js
+++ src/order.js
@@ -1,14 +1,41 @@
-function takeOrder(order1, order2, order3) {
-  var orders = [{}];
-  return orders.order1;
+// function takeOrder(orderNumber, deliveryOrders) {
+//   deliveryOrders.push(orderNumber);
+//   return deliveryOrders;
+// }
+
+function takeOrder(orderNumber, deliveryOrders) {
+  if (deliveryOrders.length <= 2) {
+    deliveryOrders.push(orderNumber);
+  } else {
+    return deliveryOrders;
+  }
 }
 
 
+function refundOrder(orderNumber, deliveryOrders) {
+  deliveryOrders.filter(1657);
+}
+// how to search by order number??
+
+
+
+// var deliveryOrders = [order1, order2, order3];
+//  var items = listItems(deliveryOrders);
+
+//  assert.deepEqual(items, "burger, blt sandwich, rueben");
+
+function listItems(deliveryOrders) {
+  var itemsList = ''
+  for (i = 0; i < deliveryOrders.length; i++) {
+    items += deliveryOrders[i].item;
+  } 
+  return itemsList 
+}
 
 
 module.exports = {
   takeOrder,
-  // refundOrder,
-  // listItems,
+  refundOrder,
+  listItems,
   // searchOrder
 }
